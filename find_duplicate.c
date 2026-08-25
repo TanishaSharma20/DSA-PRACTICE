58b15716c1f592308dfb42fd89c8d32ac71f2d94
Tanisha Sharma <vasisthtanisha2004@gmail.com>
2026-08-25 23:57:12 +0530

    Add files via upload

---
+++ find_duplicate.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int n;
+    
+    printf("enter the size of the array: ");
+    scanf("%d", &n);
+    int b = 0;
+
+    int a[n];
+    printf("enter the elements:");
+//[1,2,3,4,5] s = 7 by index = 0
+    for(int i = 0; i< n; i++){
+        scanf("%d", &a[i]);
+
+
+    }
+    int i = 0;
+    for(i = 0; i < n; i++){
+        b = b ^ a[i];
+    }
+    for(int j = i+1; j< n; j++){
+        b = b^i;
+        }
+        printf("%d", b);
+        
+       return 0;
+}
